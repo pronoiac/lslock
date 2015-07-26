15ed5c79090cc4279fddf36b44d10ece50fc3d92
James Young <pronoiac@gmail.com>
2015-07-26 15:48:40 -0700

    Adding shebang, and passing filename as argument

--- flocker.rb
+++ flocker.rb
@@ -1,17 +1,19 @@
-filename = "test"
+#!/usr/bin/env ruby
+
+filename = ARGV[0] || "test"
 
 # let's avoid opening the file as writable if it exists - updates mtime
 mode = (File.exists?(filename) ? "r" : "w")
 
 file = File.open(filename, mode)
 
-print "Acquiring lock... "
+print "Acquiring lock on filename \"#{filename}\"... "
 # open shared lock
 file.flock(File::LOCK_SH)
 puts "done."
 
 puts "Press enter to exit and free lock."
-waiting = gets
+waiting = STDIN.gets
 
 # note: on ctrl-c, lock is freed on exit anyway.
 print "Unlocking file... "
