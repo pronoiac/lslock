b39026c2dc60c9bfa532696db0868948d7f8b669
James Young <pronoiac@gmail.com>
2015-07-26 18:52:42 -0700

    Handling directory creation, adding a recursion test, calling lslock

--- lslock-test.rb
+++ lslock-test.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 
 dir_name = "/tmp/lslock-test"
-Dir.mkdir(dir_name)
-filenames = ["file1", "file2", "file3"]
+Dir.mkdir(dir_name) unless File.directory?(dir_name)
+recurse_dir_name = File.join(dir_name, "test")
+# just to verify directory recursion
+Dir.mkdir(recurse_dir_name) unless File.directory?(recurse_dir_name)
+
+filenames = ["file1", "file2", "file3", "test/recursion"]
+file_handles = []
 
 filenames.each do |filename|
-  # let's avoid opening the file as writable if it exists - updates mtime
-  mode = (File.exists?(filename) ? "r" : "w")
+  filepath = File.join(dir_name, filename)
 
-  file = File.open(filename, mode)
+  file = File.open(filepath, "w")
+  file_handles << file
 
   print "Acquiring lock on filename \"#{filename}\"... "
   # open shared lock
@@ -16,13 +21,16 @@ filenames.each do |filename|
   puts "done."
 end # locking all filenames
 
+puts("\nThis should list locks:\n\n")
 
-puts "Press enter to exit and free lock."
+system("./lslock.rb /tmp/lslock-test")
+
+puts "\nPress enter to exit and free locks."
 waiting = STDIN.gets
 
-filenames.each do |filename|
+print "Unlocking files... "
+file_handles.each do |file|
   # note: on ctrl-c, lock is freed on exit anyway.
-  print "Unlocking file... "
   file.flock(File::LOCK_UN)
-  puts "done."
 end
+puts "done. Bye!"
