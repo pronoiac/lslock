0768b2fe7e8cd12552046868740ac0cd278dde4e
James Young <pronoiac@gmail.com>
2015-07-26 18:23:21 -0700

    lslock-test - acquiring multiple locks

--- lslock-test.rb
+++ lslock-test.rb
@@ -1,21 +1,28 @@
 #!/usr/bin/env ruby
 
-filename = ARGV[0] || "test"
+dir_name = "/tmp/lslock-test"
+Dir.mkdir(dir_name)
+filenames = ["file1", "file2", "file3"]
 
-# let's avoid opening the file as writable if it exists - updates mtime
-mode = (File.exists?(filename) ? "r" : "w")
+filenames.each do |filename|
+  # let's avoid opening the file as writable if it exists - updates mtime
+  mode = (File.exists?(filename) ? "r" : "w")
 
-file = File.open(filename, mode)
+  file = File.open(filename, mode)
 
-print "Acquiring lock on filename \"#{filename}\"... "
-# open shared lock
-file.flock(File::LOCK_SH)
-puts "done."
+  print "Acquiring lock on filename \"#{filename}\"... "
+  # open shared lock
+  file.flock(File::LOCK_SH)
+  puts "done."
+end # locking all filenames
+
 
 puts "Press enter to exit and free lock."
 waiting = STDIN.gets
 
-# note: on ctrl-c, lock is freed on exit anyway.
-print "Unlocking file... "
-file.flock(File::LOCK_UN)
-puts "done."
+filenames.each do |filename|
+  # note: on ctrl-c, lock is freed on exit anyway.
+  print "Unlocking file... "
+  file.flock(File::LOCK_UN)
+  puts "done."
+end
